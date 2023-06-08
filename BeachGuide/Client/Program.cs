d32729a59d3e33ce6242696ca3e9b1af5ff94c88
nilesh-mindbowser <nilesh.p@mindbowser.com>
2023-06-08 19:59:22 +0530

    Commit For RentalUnit

--- BeachGuide/Client/Program.cs
+++ BeachGuide/Client/Program.cs
@@ -1,11 +1,16 @@
 using BeachGuide.Client;
+using BeachGuide.Client.Pages;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Localization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IStringLocalizer<HomePage>, StringLocalizer<HomePage>>();
+builder.Services.AddLocalization();
+
 
 await builder.Build().RunAsync();
